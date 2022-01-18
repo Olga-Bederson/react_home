7575019418def05e51fc33775420b66933400d93
Olga-Bederson <osbederson@gmail.com>
2022-01-18 11:42:45 +0500

    lesson1

---
+++ components/index.js
@@ -0,0 +1,12 @@
+import { Message } from './components'
+
+const text = 'Hello!'
+
+export const Homework = () => {
+    return (
+        <div>
+            <h1>Hello</h1>
+            <Message text={text} />
+        </div>
+    )
+}
